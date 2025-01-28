cbce75f92ceea970dd3085c57ea6032a57341904
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-28 17:05:07 +0000

    Done

--- Basics/Operations.js
+++ Basics/Operations.js
@@ -6,6 +6,14 @@ console.log(3 * 5)
 console.log(15 / 3)
 console.log(18 % 2)
 console.log("----------------------------------------")
+console.log(5 > 3)
+console.log(6 < 3)
+console.log("18" >  2)
+console.log("18" <  2)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log("----------------------------------------")
 
 //Tricky operations 
 // {o/p will be converted as per the first i/p}
@@ -20,4 +28,8 @@ let str1 = "Chinmay"
 let str2 = " Jadhav"
 let str3 = str1 + str2
 console.log(str3) 
+console.log("----------------------------------------")
+
+console.log("2" ===  2) // [===] it checks the datatype on both sides
+console.log(2 ===  2)
 console.log("----------------------------------------")
