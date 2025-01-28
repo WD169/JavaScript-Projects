8e86fef733eb41c7c7e815f3bf5ad5ebe1e5c8c4
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-28 15:51:01 +0000

    Operations done

---
+++ Basics/Operations.js
@@ -0,0 +1,23 @@
+//Basics operations
+
+console.log(2 + 3)
+console.log(10 - 4)
+console.log(3 * 5)
+console.log(15 / 3)
+console.log(18 % 2)
+console.log("----------------------------------------")
+
+//Tricky operations 
+// {o/p will be converted as per the first i/p}
+
+console.log("1" + 2 + 5)     // output will be converted to string o/p : 125
+console.log(1 + 2 + "3")     // o/p --> 33
+console.log(1 + "2" + 3)     // o/p --> 123
+
+
+//Concate two strings
+let str1 = "Chinmay"
+let str2 = " Jadhav"
+let str3 = str1 + str2
+console.log(str3) 
+console.log("----------------------------------------")
