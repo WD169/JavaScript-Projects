9c8918a3fe36081261841c8279c3c56f6cf131cd
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-28 15:08:10 +0000

    datatypes donee

---
+++ Basics/const_let.js
@@ -0,0 +1,13 @@
+const id = "123"                  //cannot change the value 
+let email = "chinu@gmail.com"   
+var city = "Mumbai"                 //Perfered not to use
+random = "23234"
+
+/* 
+id = 234
+we cannot change constant (const) values  
+*/  
+
+console.log(id)
+
+console.table([id,email,city,random])
