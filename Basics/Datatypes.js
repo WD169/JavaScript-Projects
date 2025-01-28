97d2c03e37c568d10ab7a2d0946544d7aac9821b
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-28 15:06:39 +0000

    DataTypes done

---
+++ Basics/Datatypes.js
@@ -0,0 +1,20 @@
+
+
+let name = "Chinu"
+let age = 22
+let Bool = false
+let notDefined;
+
+/* 
+Datatypes :
+string 
+int ---> 2 ^ 53
+bigInt
+boolean  
+null ----> it is object, it doesn't have any value { standalone value }
+undefined
+*/
+
+console.table([name,age,Bool,notDefined])
+console.table([typeof name, typeof age, typeof Bool, typeof notDefined])
+console.log(typeof null)
