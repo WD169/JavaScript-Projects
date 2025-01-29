a4f83e297c07e6a50219586692caad91d945dda9
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-29 18:05:24 +0000

    Date  DONE

---
+++ Basics/Date.js
@@ -0,0 +1,16 @@
+let MyDate = new Date()
+
+console.log(MyDate)
+console.log(MyDate.getDate())
+console.log(MyDate.getDay())
+console.log(MyDate.getHours())
+console.log(MyDate.getMonth())
+console.log(MyDate.getTime())
+
+console.log("----------------------------------------")
+
+console.log(MyDate.toLocaleDateString())
+console.log(MyDate.toTimeString())
+console.log(MyDate.toString())
+
+console.log("----------------------------------------")
