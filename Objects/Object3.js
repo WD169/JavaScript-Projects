0fde6c01b469f181360ebfce2d0611f04891eb5e
Chinmay Jadhav <105011512+WD169@users.noreply.github.com>
2025-01-30 18:09:21 +0000

    Object3 DONE

---
+++ Objects/Object3.js
@@ -0,0 +1,12 @@
+
+const new1 = {
+    emp_name: "Emp1",
+    emp_salary: 500000,
+    emp_id: 213
+}
+
+const {emp_name} = new1             //using this for retrive the information 
+const {emp_salary: salary} = new1 
+console.log(emp_name)
+console.log(salary)
+console.log("------------------------------------")
